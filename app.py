c932c9a22ca676cd34c3a13d8e3d7567f6f8de66
Natnael <ninatina423@gmail.com>
2024-12-13 22:14:59 +0300

    update

--- app.py
+++ app.py
@@ -31,21 +31,58 @@ def details(address):
     except:
         l["Address"] = None
     try:
-        l["Number of Reviews"] = soup.find("a", {"class": "y-css-1ijjqcc"}).text
+        l["Number of Reviews"] = soup.find("a", {"class": "y-css-1x1e1r2"}).text
     except:
         l["Number of Reviews"] = 0
     try:
         l["Hours of Operation"] = soup.find("div", {"class": "y-css-4dn0rh"}).text.replace("Closed", "").replace("Open", "")
     except:
         l["Hours of Operation"] = None
+
     try:
-        l["Amenities"] = soup.find_all('div', {"class": 'arrange-unit__09f24__rqHTg arrange-unit-fill__09f24__CUubG y-css-mhg9c5'})[0].text
+
+        review_list = soup.find("section", {"class": " y-css-15jz5c7"})
+        print(review_list)
+        review_texts = []
+
+        if review_list:
+            # Find all p tags within the ul
+            reviews = review_list.find_all("p", {"class": "comment__09f24__D0cxf y-css-1wfz87z"})
+            print(reviews)
+            for review in reviews:
+                try:
+                    # Find the span inside the p tag
+                    span = review.find("span", {"class": "raw__09f24__T4Ezm"})
+                    if span:
+                        review_texts.append(span.get_text(strip=True))
+                except Exception as e:
+                    print(f"Error extracting review: {e}")
+
+        # If reviews are found, add them; otherwise, set "No Reviews"
+        l["Reviews"] = review_texts if review_texts else "No Reviews"
+    except Exception as e:
+        print(f"Error finding reviews: {e}")
+        l["Reviews"] = None
+
+
+    try:
+        amenities = soup.find_all('div', {"class": 'arrange__09f24__LDfbs gutter-2__09f24__CCmUo layout-wrap__09f24__GEBlv layout-2-units__09f24__PsGVW y-css-mhg9c5'})
+        amenities_list = []
+        for amenity in amenities:
+            spans = amenity.find_all("span", {"class": ["y-css-19xonnr", "y-css-jbomhy"]})
+            for span in spans:
+                if span:
+                    amenities_list.append(span.text)
+        l["Amenities"] = ", ".join(amenities_list)
+
+    
     except:
         l["Amenities"] = None
 
+    
+
     if l["Number of Reviews"] != 0:
         l["Number of Reviews"] = l["Number of Reviews"].replace("(", "").replace(")", "")
-
     data.append(l)
     return data
 
@@ -74,6 +111,7 @@ if st.button("Scrape"):
         st.markdown(f"**Number of Reviews:** {result['Number of Reviews']}")
         st.markdown(f"**Hours of Operation:** {result['Hours of Operation']}")
         st.markdown(f"**Amenities:** {result['Amenities']}")
+        st.markdown(f"**Reviews:** {result['Reviews']}")
 
 
     df = pd.DataFrame(all_results)
